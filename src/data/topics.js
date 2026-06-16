548e05d3bcd80bf25747652eb07590053fbd8fe2
EdgarTmc <Edgar.garzon@tramaco.com.ec>
2026-06-16 10:23:08 -0500

    fix: update topics

--- src/data/topics.js
+++ src/data/topics.js
@@ -67,7 +67,7 @@ module.exports = [
   },
   {
     id: 12,
-    title: "ING. Garzon",
+    title: "JEFE Salcedo",
     description: "Modelo donde Git es la fuente única de verdad para infraestructura.",
     link: "https://www.gitops.tech/"
   }
